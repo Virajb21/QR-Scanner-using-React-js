cba6da2516da0ded95a6a66d9d297127f8a604d8
Virajb21 <viraj.bhukte@yahoo.com>
2023-01-31 10:59:53 +0530

    initial commit

---
+++ scan/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import QRCombination from "./components/QRcombination";
+function App() {
+  return (
+    <div>
+     
+       <QRCombination />
+    </div>
+     
+  );
+}
+
+export default App;
